88a46ac70ffe200d919e4dd9c2d53d739ee010c6
DiabloZ <Diabloz@me.com>
2023-11-22 20:57:33 +0300

    Повторил урок

--- 3 семестр/Python Jango/Занятие 11/NewProject/News/urls.py
+++ 3 семестр/Python Jango/Занятие 11/NewProject/News/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from News.views import index, human, get_category, get_profession, view_news, view_person
+from News.views import index, human, get_category, get_profession, view_news, view_person, add_news
 
 urlpatterns = [
     path('', index, name='Home'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('category/<int:category_id>', get_category, name='Category'),
     path('news/<int:news_id>', view_news, name='View_news'),
     path('profession/<int:profession_id>', get_profession, name='Profession'),
-    path('person/<int:person_id>', view_person, name='View_person')
+    path('person/<int:person_id>', view_person, name='View_person'),
+    path('news/add_news', add_news, name='Add_news')
 ]
--- 3 семестр/Python Jango/Занятие 11/NewProject/News/views.py
+++ 3 семестр/Python Jango/Занятие 11/NewProject/News/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
+from .forms import NewsForm
 from .models import News, Category, Profession
 from .models import Human
 
@@ -30,6 +31,18 @@ def view_news(request, news_id):
         'news_item': news_item
     }
     return render(request, 'News/view_news.html', context=context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # news = News.objects.create(**form.cleaned_data)
+            news = form.save()
+            return redirect(news)
+    else:
+        form = NewsForm()
+    return render(request, 'News/add_news.html', context={"form": form})
 
 
 def view_person(request, person_id):
