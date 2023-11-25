b753ce041576fc06096c3fc4c0cfbc3cb9baa510
DiabloZ <Diabloz@me.com>
2023-11-25 13:51:49 +0300

    Повторил урок

--- 3 семестр/Python Jango/Занятие 12/NewProject/News/urls.py
+++ 3 семестр/Python Jango/Занятие 12/NewProject/News/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from News.views import index, human, get_category, get_profession, view_news, view_person, add_news, add_person
+from News.views import HomeNews, NewsByCategory, ViewNews, AddNews
+from News.views import human, get_profession, view_person, add_person
 
 urlpatterns = [
-    path('', index, name='Home'),
+    path('', HomeNews.as_view(), name='Home'),
     path('people', human, name='People'),
-    path('category/<int:category_id>', get_category, name='Category'),
-    path('news/<int:news_id>', view_news, name='View_news'),
+    path('category/<int:category_id>', NewsByCategory.as_view(), name='Category'),
+    path('news/<int:pk>', ViewNews.as_view(), name='View_news'),
     path('profession/<int:profession_id>', get_profession, name='Profession'),
     path('person/<int:person_id>', view_person, name='View_person'),
-    path('news/add_news', add_news, name='Add_news'),
+    path('news/add_news', AddNews.as_view(), name='Add_news'),
     path('person/add_person', add_person, name='Add_person')
 ]
