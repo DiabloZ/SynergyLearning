37c1c32171021210edb7b32fadc404c79d7fe2a1
DiabloZ <Diabloz@me.com>
2025-03-23 13:15:47 +0300

    Проработал четвертую тему

---
+++ 6semester/GoLang/firstProject/main/lesson4.go
@@ -0,0 +1,10 @@
+package main
+
+import "firstProject/math"
+
+func main() {
+	println(math.Sum(1, 2))
+	println(math.Substract(1, 2))
+	println(math.Multiply(1, 2))
+	println(math.Divide(255, 10))
+}
---
+++ 6semester/GoLang/firstProject/math/math.go
@@ -0,0 +1,17 @@
+package math
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+func Substract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+func Divide(a, b float32) float32 {
+	return a / b
+}
